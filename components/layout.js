719116be48762dd0fed04103e653c7baca322ddf
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-11-23 14:39:14 +0700

    Change main font to Roboto

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,9 @@ export default function Layout({ children }) {
         <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
             </Head>
 
             <nav className={styles.nav}>
