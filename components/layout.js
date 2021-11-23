c554b7aa32e6c0877aa567d4937e00454f28c06c
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-11-23 14:32:33 +0700

    Add home page

---
+++ components/layout.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from './layout.module.css'
+
+export const siteTitle = '25LabStudio'
+
+export default function Layout({ children }) {
+    return(
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <nav className={styles.nav}>
+                <div className={styles.logo}>
+                    <Link href="/">
+                        {''}
+                    </Link>
+                </div>
+                <ul className={styles["nav-right"]}>
+                    <li><Link href="/">home</Link></li>
+                    <li><Link href="/about">about</Link></li>
+                    <li><Link href="/talents">talents</Link></li>
+                    <li><Link href="/contact">contact</Link></li>
+                </ul>
+            </nav>
+
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
