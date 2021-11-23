67e0dec6dd67419d9f3147121b1eaa10f83d4033
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-11-23 14:54:41 +0700

    Change custom font link to _document.js

--- components/layout.js
+++ components/layout.js
@@ -8,10 +8,7 @@ export default function Layout({ children }) {
     return(
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+                <link rel="icon" href="/favicon.ico" />                
             </Head>
 
             <nav className={styles.nav}>
