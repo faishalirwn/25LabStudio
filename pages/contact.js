b10314a8726b4e1befdc19b0aa6ed7db950f0769
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2021-12-04 20:40:02 +0700

    add carousel

--- pages/contact.js
+++ pages/contact.js
@@ -1,31 +1,45 @@
-import Layout, {siteTitle} from "../components/layout"
-import Head from 'next/head'
-import styles from '../styles/Contact.module.css'
+import Layout, { siteTitle } from "../components/layout";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Contact.module.css";
 
 export default function Contact() {
-    return (
-        <Layout>
-            <Head>
-                <title>Contact - {siteTitle}</title>
-            </Head>
-            
-            <div className={styles["contact-container"]}>
-                <div></div>
-                <div className={styles.rect}></div>
-                <div className={styles.contact}>
-                    <h1>Contact</h1>
-                    <p>email@gmail.com</p>
-                    <p>Jl. Baros, Sukataris, Karangtengah, Kabupaten Cianjur, Jawa Barat 43281</p>
-                </div>
-                <footer className={styles.footer}>
-                    <div>
-                        <a className={styles["social-item"]} href="https://www.instagram.com/25labstudio/" target="_blank" rel="noopener noreferrer">
-                            <img src="/Instagram_simple_icon.svg" alt="Instagram Icon" />
-                        </a>
-                    </div>
-                    <p>© 2021 25LabStudio</p>                    
-                </footer>
-            </div>
-        </Layout>        
-    )
+	return (
+		<Layout>
+			<Head>
+				<title>Contact - {siteTitle}</title>
+			</Head>
+
+			<div className={styles["contact-container"]}>
+				<div></div>
+				<div className={styles.rect}></div>
+				<div className={styles.contact}>
+					<h1>Contact</h1>
+					<p>email@gmail.com</p>
+					<p>
+						Jl. Baros, Sukataris, Karangtengah, Kabupaten Cianjur, Jawa Barat
+						43281
+					</p>
+				</div>
+				<footer className={styles.footer}>
+					<div>
+						<a
+							className={styles["social-item"]}
+							href="https://www.instagram.com/25labstudios/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								src="/Instagram_simple_icon.svg"
+								alt="Instagram Icon"
+								height={33}
+								width={33}
+							/>
+						</a>
+					</div>
+					<p>© 2021 25LabStudio</p>
+				</footer>
+			</div>
+		</Layout>
+	);
 }
